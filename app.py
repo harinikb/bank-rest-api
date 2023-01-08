e27730d29f6b323a11bfdc72b09c0877f4a0013a
harinikb <harinikb2002@gmail.com>
2023-01-08 18:30:31 +0530

    Updated to use sqlalchemy models instead of raw queries

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.ext.declarative import declarative_base
 
 rest_api_app = Flask(__name__)
 rest_api_app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin@localhost:5432/banks_dump"
 db_handler = SQLAlchemy(rest_api_app)
 migrate = Migrate(rest_api_app, db_handler)
+Base = declarative_base()
+Column = db_handler.Column
 
 
 class Bank(db_handler.Model):
@@ -13,6 +16,19 @@ class Bank(db_handler.Model):
 
     id = db_handler.Column(db_handler.Integer, primary_key=True)
     name = db_handler.Column(db_handler.String())
+
+
+class BankBranches(db_handler.Model):
+    __tablename__ = 'bank_branches'
+
+    ifsc = Column(db_handler.Integer, primary_key=True)
+    bank_id = Column(db_handler.Integer)
+    branch = Column(db_handler.String(300))
+    address = Column(db_handler.String(300))
+    city = Column(db_handler.String(300))
+    district = Column(db_handler.String(300))
+    state = Column(db_handler.String(300))
+    bank_name = Column(db_handler.String(300))
 
 
 @rest_api_app.route("/banks", methods=["GET"])
@@ -38,30 +54,37 @@ def bank_details(bank_id):
 
 @rest_api_app.route("/banks/<bank_id>/branches", methods=["GET"])
 def bank_branches(bank_id):
-    session = db_handler.session()
-    cursor = session.execute(
-        f"SELECT * FROM bank_branches WHERE bank_id = {bank_id};", ).cursor
-    branches = cursor.fetchall()
-    print(branches)
+    branches = BankBranches.query.filter_by(bank_id=bank_id).all()
 
     return [{
-        "ifsc": branch[0],
-        "bank_id": branch[1],
-        "branch": branch[2],
-        "address": branch[3],
-        "city": branch[4],
-        "district": branch[5],
-        "state": branch[6],
-        "bank_name": branch[7]
-    } for branch in branches]
+        "ifsc": branch.ifsc,
+        "bank_id": branch.bank_id,
+        "branch": branch.branch,
+        "address": branch.address,
+        "city": branch.city,
+        "district": branch.district,
+        "state": branch.state,
+        "bank_name": branch.bank_name
+    }for branch in branches
+
+    ]
 
 
 @rest_api_app.route("/banks/<bank_id>/branches/<branch_id>", methods=["GET"])
 def bank_branch(bank_id, branch_id):
-    session = db_handler.session()
-    cursor = session.execute(
-        f"SELECT * FROM bank_branches WHERE bank_id = {bank_id} AND ifsc = '{branch_id}';", ).cursor
-    b = cursor.fetchall()
+    branch = BankBranches.query.filter_by(
+        bank_id=bank_id, ifsc=branch_id).first_or_404()
+
+    return {
+        "ifsc": branch.ifsc,
+        "bank_id": branch.bank_id,
+        "branch": branch.branch,
+        "address": branch.address,
+        "city": branch.city,
+        "district": branch.district,
+        "state": branch.state,
+        "bank_name": branch.bank_name
+    }
 
     return [{
         "ifsc": row[0],
